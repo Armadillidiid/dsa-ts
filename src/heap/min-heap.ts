e038094454fc6e4f3250d46f97a9ff43ffafa885
Emmanuel Isenah <emmanuelisenah@gmail.com>
2025-10-28 16:52:23 +0100

    feat: add heap

---
+++ src/heap/min-heap.ts
@@ -0,0 +1,37 @@
+class MinHeap {
+  heap: number[];
+
+  constructor(heap: number[] = []) {}
+
+  add(num: number) {}
+
+  peek(): number | undefined {}
+
+  poll(): number | undefined {}
+
+  isEmpty(): boolean {}
+
+  toString(): string {}
+
+  heapifyUp(): void {}
+
+  heapifyDown(): void {}
+
+  getLeftChildIndex(parentIndex: number): number {}
+
+  getRightChildIndex(parentIndex: number): number {}
+
+  getParentIndex(childIndex: number): number {}
+
+  hasLeftChild(parentIndex: number): boolean {}
+
+  hasRightChild(parentIndex: number): boolean {}
+
+  leftChild(parentIndex: number): number {}
+
+  rightChild(parentIndex: number): number {}
+
+  swap(indexOne: number, indexTwo: number): void {}
+}
+
+export default MinHeap;
